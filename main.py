3ea4f03d81db955b1e5f5a4362815453147f157f
knyjs0710 <127165374+knyjs0710@users.noreply.github.com>
2023-05-16 12:41:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-df = pd.DataFrame(np.randon.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
+df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],columns=['lat','lon'])
 st.map(df)
